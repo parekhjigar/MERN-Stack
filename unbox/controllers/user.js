853b38e6a0dfb800d1f434d180e84fcfa8f23359
parekhjigar <hello@parekhjigar.com>
2020-03-26 23:31:17 +1100

    Adding route for signup

--- unbox/controllers/user.js
+++ unbox/controllers/user.js
@@ -1,3 +1,16 @@
-exports.test = (req, res) => {
-	res.json({ message: "Testing controller"});
+const User = require('../models/user');
+
+exports.signup = (req, res) => {
+	console.log("req.body", req.body);
+	const user = new User(req.body);
+	user.save((err, user) => {
+		if(err) {
+			return res.status(400).json({
+				error
+			});
+		}
+		res.json({
+			user
+		});
+	});
 };
--- unbox/routes/user.js
+++ unbox/routes/user.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router()
 
 
-const {test} = require("../controllers/user");
+const { signup } = require("../controllers/user");
 
-router.get("/", test);
+router.post("/signup", signup);
 
 module.exports = router;
