664dcf47299e8a80b02774b238d27296e2aeb24a
parekhjigar <hello@parekhjigar.com>
2020-04-04 23:18:44 +1100

    Added a test route

--- unbox/routes/user.js
+++ unbox/routes/user.js
@@ -2,11 +2,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, signin, signout } = require("../controllers/user");
+const { signup, signin, signout, requireSignin } = require("../controllers/user");
 const { userSignupValidator } = require("../validator");
 
 router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
 router.get("/signout", signout);
 
+router.get("/test", requireSignin, (req, res) => {
+	res.send("It works!");
+});
+
 module.exports = router;
+
+
