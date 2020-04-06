ef12519df0ffdb150b0b58cbf615ad742a794ca2
parekhjigar <hello@parekhjigar.com>
2020-04-06 23:46:27 +1000

    Added route for user

--- unbox/app.js
+++ unbox/app.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 // Importing routes
 const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
 
 // app
 const app = express();
@@ -29,6 +30,7 @@ app.use(expressValidator());
 
 // routes middleware
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000
 
