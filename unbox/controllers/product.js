4f1f37e9ce2667a1fad43ce08ec9b574cd381f6e
parekhjigar <hello@parekhjigar.com>
2020-04-16 23:15:37 +1000

    Added controller to delete product

--- unbox/controllers/product.js
+++ unbox/controllers/product.js
@@ -66,3 +66,21 @@ exports.create = (req, res) => {
 	});
 
 };
+
+
+exports.remove = (req, res) => {
+	let product = req.product
+	product.remove((err, deletedProduct) => {
+		if (err) {
+				return res.status(400).json({
+					error: errorHandler(error)
+				});
+			}
+			res.json({
+				deletedProduct,
+				"message": "Product deleted successfully!"
+			})
+	});
+}
+
+
