4c6f24e12a7462bb0bc424084c8d9ec69be19e50
parekhjigar <hello@parekhjigar.com>
2020-04-17 20:10:16 +1000

    Added controller for categroryById and read category

--- unbox/controllers/category.js
+++ unbox/controllers/category.js
@@ -2,6 +2,20 @@
 const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+
+exports.categoryById = (req, res, next, id) => {
+	Category.findById(id).exec((err, category) => {
+		 if(err || !category) {
+			return res.status(400).json({
+				error: "Category not found!"
+			});
+		}
+		req.category = category;
+		next();
+	});
+}
+
+
 exports.create = (req, res) => {
 	const category = new Category(req.body)
 	category.save((err, data) => {
@@ -13,3 +27,8 @@ exports.create = (req, res) => {
 		res.json({ data });
 	});
 };
+
+
+exports.read = (req, res) => {
+	return res.json(req.category);
+}
