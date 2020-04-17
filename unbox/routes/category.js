9804d95a57e2a03cc2936e453b702eb4e16623ca
parekhjigar <hello@parekhjigar.com>
2020-04-17 20:27:32 +1000

    Added route to delete category

--- unbox/routes/category.js
+++ unbox/routes/category.js
@@ -9,7 +9,7 @@ const { userById } = require("../controllers/user");
 router.get("/category/:categoryId", read)
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
 router.put("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, update);
-
+router.delete("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, remove);
 
 router.param("categoryId", categoryById);
 router.param("userId", userById);
