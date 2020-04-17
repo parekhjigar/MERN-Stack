f14c6aadea18cdc34a0b151ade4afb308d26372e
parekhjigar <hello@parekhjigar.com>
2020-04-17 20:27:15 +1000

    Added controller to delete category

--- unbox/controllers/category.js
+++ unbox/controllers/category.js
@@ -47,3 +47,17 @@ exports.update = (req, res) => {
 	});
 }
 
+
+exports.remove = (req, res) => {
+	const category = req.category
+	category.remove((err, data) => {
+		if(err) {
+			return res.status(400).json({
+				error: errorHandler(err)
+			});
+		}
+		res.json({
+			message: "Category deleted successfully!"
+		});
+	});
+}
