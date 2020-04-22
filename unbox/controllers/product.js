86c546974173b2c1c389d3713c91ea7143d278f4
parekhjigar <hello@parekhjigar.com>
2020-04-22 18:05:00 +1000

    Added controller to list products by search

--- unbox/controllers/product.js
+++ unbox/controllers/product.js
@@ -188,3 +188,46 @@ exports.listCategories = (req, res) => {
 		res.json(categories)
 	})
 }
+
+
+// Listing products by search 
+exports.listBySearch = (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+ 
+
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {            
+                // Filter according to price where gte is greater than and lte is less then
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+ 
+    Product.find(findArgs)
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, order]])
+        .skip(skip)
+        .limit(limit)
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Products not found"
+                });
+            }
+            res.json({
+                size: data.length,
+                data
+            });
+        });
+};
