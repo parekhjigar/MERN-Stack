68b447044d3a6fc8c20d2d054b1207bf2b2dc07b
parekhjigar <hello@parekhjigar.com>
2020-04-28 01:26:24 +1000

    Rendered routes

--- unbox-front/src/index.js
+++ unbox-front/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './Routes';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render( 
+  <Routes />,
   document.getElementById('root')
 );
