4857f103ffb4e17c892ef70cf0123e0cafcea7a3
parekhjigar <hello@parekhjigar.com>
2020-04-29 07:21:57 +1000

    Added private route for user login

--- unbox-front/src/Routes.js
+++ unbox-front/src/Routes.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Signup from './user/Signup'
-import Signin from './user/Signin'
-import Home from './core/Home'
-import Menu from './core/Menu'
+import React from "react"
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Home from "./core/Home";
+import PrivateRoute from "./auth/PrivateRoute";
+import Dashboard from "./user/UserDashboard"
 
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Menu />
-            
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
+                <PrivateRoute path="/dashboard" exact component={Dashboard}/>
             </Switch>
         </BrowserRouter>
 
