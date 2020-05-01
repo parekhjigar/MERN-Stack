3227b8c4576b589860e5d25f1ad2b00d081cc040
parekhjigar <hello@parekhjigar.com>
2020-05-01 16:23:57 +1000

    Added sections for user information

--- unbox-front/src/user/UserDashboard.js
+++ unbox-front/src/user/UserDashboard.js
@@ -18,17 +18,17 @@ const Dashboard = () => {
                         </Link>
                     </li>
                     <li className="list-group-item">
-                    <Link className="nav-link" to="/profile/update">
-                        Update Profile
-                    </Link>
+                        <Link className="nav-link" to="/profile/update">
+                            Update Profile
+                        </Link>
                     </li>
                 </ul>
             </div>
-        )
-    }
+        );
+    };
 
-    return (
-        <Layout title="Dashboard" description="User Dashboard" className="container">
+    const userInfo = () => {
+        return (
             <div className="card mb-5">
                 <h3 className="card-header">User Details</h3>
                 <ul className="list-group">
@@ -37,13 +37,37 @@ const Dashboard = () => {
                     <li className="list-group-item">Role: {role === 1 ? "Admin" : "Registered User"}</li>
                 </ul>
             </div>
-            
+        );
+    };
+
+    const purchaseHistory = () => {
+        return (
             <div className="card mb-5">
                 <h3 className="card-header">Purchase history</h3>
                 <ul className="list-group">
                     <li className="list-group-item">History</li>
                 </ul>
             </div>
+        );
+    };
+
+    return (
+        <Layout
+            title="Dashboard"
+            description={`Welcome ${name}!`}
+            className="container-fluid"
+        >
+            <div className="row">
+                <div className="col-3">
+                    { userLinks() }
+                </div>
+
+                <div className="col-9">
+                    { userInfo() }
+                    { purchaseHistory() }
+                </div>
+            </div>
+            
         </Layout>
     );
 };
