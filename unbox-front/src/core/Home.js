e7e24ff3d0ff8527465a4c573ad9cc8847102475
parekhjigar <hello@parekhjigar.com>
2020-05-03 02:16:04 +1000

    Initialized component for home

--- unbox-front/src/core/Home.js
+++ unbox-front/src/core/Home.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React from "react";
+import Layout from "./Layout";
 
-const Home = () => <div>Home test</div>
+const Home = () => (
+    <Layout title="Home Page" description="Unbox diary market">
+        
+    </Layout>
+);
 
 export default Home;
