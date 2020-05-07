2dfe21c96af32ee52351d2e14bf43b3ef0145dea
parekhjigar <hello@parekhjigar.com>
2020-05-07 20:59:48 +1000

    Added component for diary card

---
+++ unbox-front/src/core/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Card = ({product}) => {
+    return (
+        <div className="col-4 mb-3">
+            <div className="card">
+                <div className="card-header">{product.name}</div>
+                <div className="card-body">{product.name}
+                    <p>{product.description}</p>
+                    <p>{product.price}</p>
+                    <Link to="/">
+                        <button className="btn btn-outline-primary mt-2 mb-2 card-btn-1">
+                            View Product
+                        </button>
+                    </Link>
+                    <button className="btn btn-outline-warning mt-2 mb-2 card-btn-1  ">
+                        Add to cart
+                    </button>
+                </div>               
+            </div>
+        </div>
+    );
+};
+
+export default Card; 
