b2e916e5e4e216a2bbf337d4bb31a6a2561e1dd7
parekhjigar <hello@parekhjigar.com>
2020-05-16 23:36:24 +1000

    Added controller to search and added route

--- unbox/routes/product.js
+++ unbox/routes/product.js
@@ -9,6 +9,7 @@ const {
 		remove,
 		update,
 		list,
+		listSearch,
 		listRelated,
 		listCategories,
 		listBySearch,
@@ -29,7 +30,9 @@ router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove);
 router.put("/product/:productId/:userId", requireSignin, isAuth, isAdmin, update)
+
 router.get("/products", list);
+router.get("/products/search", listSearch);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
 router.post("/products/by/search", listBySearch);
